34ba44a1722fe2a71ecf8f2e88d5e93c5bed759a
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-04-04 21:41:19 +0200

    Create area.py
    This will be a script of functions that can find the area of different shapes.

---
+++ werk/area.py
@@ -0,0 +1,15 @@
+#module to find area of shapes
+
+#functions should be called as such: name(number, number)
+
+import math
+
+def triangle(base, height):
+    return base*height/2
+
+
+def rectangle(base, height):
+    return base*height
+  
+#circles?
+#what other shapes to think of?
