53387c7baf7073a4f49ffd4458266716d3a96055
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-05-08 17:36:49 +0200

    Add files via upload
    python file that helps picks movies

---
+++ play/movie_night.py
@@ -0,0 +1,24 @@
+#movie night
+import random
+import sys
+
+movies = ['Action', 'Adventure', 'Anime', 'Classic', 'Comedy', 'Horror', 'Thriller']
+number1 = random.randint(1, 15)
+number2 = random.randint(1, 20)
+
+while True:
+    print('------------'*3)
+    print("Movie Picker!!")
+    while True:
+        print("Would you like to watch a movie?? (y, n)")
+        word = input()
+        if word == 'n':
+            sys.exit()
+        if word == 'y':
+            break
+    print('Go to genre: ',random.choice(movies))
+    print('Go down {} rows'.format(number1))
+    print('And go right {} times'.format(number2))
+    print('This is your movie!!')
+
+    print("---------"*3)
