7769f2da635627105850e404f3d3ae40c2b48190
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-05-08 17:37:28 +0200

    Update movie_night.py

--- play/movie_night.py
+++ play/movie_night.py
@@ -7,7 +7,7 @@ number1 = random.randint(1, 15)
 number2 = random.randint(1, 20)
 
 while True:
-    print('------------'*3)
+    print('================'*3)
     print("Movie Picker!!")
     while True:
         print("Would you like to watch a movie?? (y, n)")
@@ -21,4 +21,4 @@ while True:
     print('And go right {} times'.format(number2))
     print('This is your movie!!')
 
-    print("---------"*3)
+    print("================"*3)
