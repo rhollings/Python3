dffe219ba7b33e5bb39807c7704ebd4a1b02bd51
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-05-30 22:02:25 +0200

    Create json01.py

---
+++ werk/json01.py
@@ -0,0 +1,11 @@
+import json
+
+#learning to import/call/return json data
+# some JSON:
+x = '{ "name":"John", "age":30, "city":"New York"}'
+
+# parse x:
+y = json.loads(x)
+
+# the result is a Python dictionary:
+print(y["age"])
