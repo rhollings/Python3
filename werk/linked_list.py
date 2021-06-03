091a45401a90bb76385803384841bad8315b340c
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-06-03 10:09:56 +0200

    Create linked_list.py
    created a practice linked list

---
+++ werk/linked_list.py
@@ -0,0 +1,14 @@
+class Node:
+  data = None
+  next_node = None
+  
+  def __init__(self, data):
+    self.data = data
+    
+  def __repr__(self):
+    return "<Node data: %s>" % self.data
+  
+class LinkedList:
+  
+  def __init__(self):
+    self.head = None
