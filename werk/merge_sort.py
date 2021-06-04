3eedf7cc6c07bf78018c83b69aeea28c48c3a8f0
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-06-04 16:57:51 +0200

    Update merge_sort.py

--- werk/merge_sort.py
+++ werk/merge_sort.py
@@ -15,3 +15,42 @@ def split(list):
   left = list[:mid]
   right = list[mid:]
   
+  return left, right
+
+def merge(left, right):
+  
+  l = []
+  i = 0
+  j = 0
+  
+  while i < len(left) and j < len(right):
+    if left[i] < right[j]:
+      l.append(left[i])
+      i += 1
+    else:
+      l.append(right[j])
+      j += 1
+      
+  while i < len(left):
+    l.append(left[i])
+    i += 1
+    
+  while j < len(right):
+    l.append(right[j])
+    j += 1
+    
+    return l
+  
+def verify_sorted(list):
+  n = len(list)
+  
+  if n == 0 or n == 1:
+    return True
+  
+  return list[0] < list[1] and verify_sorted(list[1:])
+    
+alist = [23, 15, 27, 12, 3, 44, 92, 56, 4, 78, 34]
+blist = [94, 1, 46, 30, 7, 72, 10, 14, 17, 22]
+
+l = merge_sort(alist)
+print(l)
