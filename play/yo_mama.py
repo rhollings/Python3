487e867ecaf7c7bb3a9f8bed660a06a84e052c2f
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-08-20 14:25:12 +0200

    Add files via upload
    scripts that tells "yo mama" jokes

---
+++ play/yo_mama.py
@@ -0,0 +1,22 @@
+import sys, requests
+
+response = requests.get("https://yomomma-api.herokuapp.com/jokes")
+joke = response.json()
+while True:
+    print('Welcome....')
+    print('=====================')
+
+    while True:
+        print("Would you like a 'yo mama' joke?")
+        answer = input("y or n:  ")
+
+        if answer == 'y':
+            print('____________________')
+            print(joke['joke'])
+            print('=====================')
+
+
+        else:
+            answer == 'n'
+            print('Goodbye...')
+            sys.exit()
