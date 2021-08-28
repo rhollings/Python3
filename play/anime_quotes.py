765fd5daf37323c16887e2ee100dd16b017f93c8
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-08-28 10:48:34 +0200

    Add files via upload

---
+++ play/anime_quotes.py
@@ -0,0 +1,23 @@
+"""
+url = https://animechan.vercel.app/api/random = calls a random quote
+url = https://animechan.vercel.app/api/quotes = calls ten random quotes
+url = https://animechan.vercel.app/api/quotes/anime?title=naruto = quotes by anime title
+url = https://animechan.vercel.app/api/quotes/character?name=saitama = quotes by character 
+url = https://animechan.vercel.app/api/available/anime = all available anime
+"""
+
+import urllib.request, json
+
+url = "https://animechan.vercel.app/api/random"
+url2 = "https://animechan.vercel.app/api/available/anime"
+
+
+response = urllib.request.urlopen(url)
+data = json.loads(response.read())
+name = data["character"]
+anime = data["anime"]
+quote = data["quote"]
+
+
+#print("In the anime", anime, ",", name, "said", quote)
+
