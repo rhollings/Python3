2342d4a6e30eae832221b070a1312ba4c7211112
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-11-03 15:58:15 +0100

    Update alphabet_soup.py

--- play/alphabet_soup.py
+++ play/alphabet_soup.py
@@ -14,12 +14,15 @@ B.) Input: "eLEPhAnt" Output: "AEehLnPt"
 # attempt 1
 
 def alphabet_soup(string):
-  li = sorted(list(string))
+  li = sorted(list(string)) # creates a list and sorts the letters
   new_string = ''
-  for char in li:
+  for char in li: #iterate thru list and add to new_string
     new_string += char
     
   return new_string
 
 word = input('Enter a String: ')
 print(aplhabet_soup(word))
+
+# attempt 2 
+
