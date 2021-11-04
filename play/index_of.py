3d314d648d0a92d6288a32a533b0222f3c1afe71
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-11-04 15:20:09 +0100

    Create index_of.py
    today's coding problem from daily byte

---
+++ play/index_of.py
@@ -0,0 +1,11 @@
+'''
+Given two strings s and t, return the index of the first occurrence of t within s if it exists; otherwise, return -1.
+
+Ex: Given the following strings s and t…
+
+s = "abc", t = "a", return 0.
+Ex: Given the following strings s and t…
+
+s = "abc", t = "abcd", return -1.
+
+'''
