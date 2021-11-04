f5397416c49e4ff47444c5eceaaa9df6ed522567
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-11-04 16:13:21 +0100

    Create jungle_book.py
    interview question that confused me

---
+++ werk/jungle_book.py
@@ -0,0 +1,10 @@
+'''
+There are a number of animal species in the jungle. Each species has one or more predators that may be direct or indirect.
+Species X is said to be a predator of species Y if at least one of the following is true:
+Species X is a direct predator of species Y. If species X is a direct predator of species Z, and Z is a direct predator of Y,
+then species X is an indirect predator of species Y. Indirect predation is transitive through any number of levels.
+Each species has a maximum of 1 direct predator. No two species will ever be mutual predators, and no species is a predator of itself. 
+Determine the minimum number of groups that must be formed to so that no species is grouped with its predators, direct or indirect.
+
+'''
+
