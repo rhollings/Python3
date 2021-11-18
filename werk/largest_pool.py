b660e2fead804005c180fb04fe13a24f4b2452c3
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-11-18 09:38:25 +0100

    Create largest_pool.py
    problem from Daily Byte

---
+++ werk/largest_pool.py
@@ -0,0 +1,17 @@
+'''
+You are building a pool in your backyard and want to create the largest pool possible.
+The largest pool is defined as the pool that holds the most water. 
+The workers you hired to dig the hole for your pool didn’t do a great job and
+because of this the depths of the pool at different areas are not equal.
+Given an integer array of non-negative integers that represents a histogram
+of the different heights at each position of the hole for your pool, 
+return the largest pool you can create.
+
+Ex: Given the following heights...
+
+heights = [1, 4, 4, 8, 2], return 8. 
+You can build your largest pool (most water) between indices 1 and 3 (inclusive) for a water volume of 4 * 2 = 8.
+'''
+
+def largest_pool():
+  #code here
