3925a9cae14c91e71200e47366067a8a30b9d5c1
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-11-18 09:33:26 +0100

    Create to_third_power.py
    algo problem from Daily Byte

---
+++ werk/to_third_power.py
@@ -0,0 +1,13 @@
+'''
+Given an integer n, return whether or not it is a power of three.
+Ex: Given the following value for n...
+
+n = 9, return true
+
+Ex: Given the following value for n...
+
+n = 50, return false
+'''
+
+def third_power():
+  #code here 
