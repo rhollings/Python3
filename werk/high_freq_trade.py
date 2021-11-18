4aecc2821ccc3a3dfddcbe88d56cdfd1905e0f3b
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-11-18 09:35:52 +0100

    Create high_freq_trade.py

---
+++ werk/high_freq_trade.py
@@ -0,0 +1,14 @@
+'''
+You are given the list of prices of a particular stock. 
+Each element in the array represents the price of the stock at
+a given second throughout the current day. Return the maximum profit
+you can make trading this stock.
+Note: You may only ever buy and sell a single share of the stock,
+but you may make multiple transactions (i.e. buys and sells).
+Ex: Given the following prices...
+
+prices = [8, 3, 2, 4, 6, 4, 5], return 5.
+''' 
+
+def freq_trade():
+  #code here
