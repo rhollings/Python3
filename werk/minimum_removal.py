728a155956447cc625a0db0efb32e2497e070904
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-11-18 09:34:47 +0100

    Create minimum_removal.py
    problem from Daily Byte

---
+++ werk/minimum_removal.py
@@ -0,0 +1,13 @@
+'''
+Given a string s, remove the minimum number of parentheses to make s valid. Return all possible results.
+
+Ex: Given the following string s...
+
+s = "(()()()", return ["()()()","(())()","(()())"].
+Ex: Given the following string s...
+
+s = "()()()", return ["()()()"].
+'''
+
+def min_remove():
+  #code here
