27e23fb9451769d6b10c11d92fc30827824e2cbe
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-11-18 09:37:05 +0100

    Create remove_value.py
    problem from Daily Byte

---
+++ werk/remove_value.py
@@ -0,0 +1,13 @@
+'''
+Given an integer array nums and a value, 
+val, remove all instances of val in-place and
+return the length of the new array.
+Note: It does not matter what values you leave in the array
+beyond the array’s new length.
+Ex: Given the following nums and val...
+
+nums = [1, 2, 3], val = 2, return 2 (after your modifications your array could look like [1, 3, 3]).
+'''
+
+def remove_val():
+  #code here
