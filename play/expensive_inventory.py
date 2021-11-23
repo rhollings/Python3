c36d6f0849c9750687ad099d3f4c900b5ec98dbb
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-11-23 20:31:33 +0100

    Create expensive_inventory.py
    today's challenge

---
+++ play/expensive_inventory.py
@@ -0,0 +1,21 @@
+'''
+You are given a list of values and a list of labels. 
+The ith element in labels represents the label of the ith element. 
+Similarly, the ith element in values represents the value associated with the ith element 
+(i.e. together, an “item” could be thought of as a label and a price).
+Given a list of values, a list of labels, a limit, and wanted, return the sum 
+of the most expensive items you can group such that the total number of 
+items used is less than wanted and the number of any given label that is used is less than limit.
+
+Ex: Given the following variables...
+
+values = [5,4,3,2,1]
+label = [1,1,2,2,3]
+wanted = 3
+limit = 1
+return 9 (the sum of the values associated with the first, third, and fifth items).
+
+'''
+
+def expensive_inventory:
+  #code here
