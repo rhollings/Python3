c9d185578bcb7d2b39c050264587b704fa3ebac6
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-11-24 16:08:01 +0100

    Create likes_what.py
    algo I 'created' (idk if it existed already) that uses 2 lists to return a single combined list as such

---
+++ play/likes_what.py
@@ -0,0 +1,27 @@
+'''
+Given two seperate lists of strings, return a list combining the two strings.
+
+Ex. str1 = ['name1', 'name2', 'name3', 'name4', 'name5']
+    str2 = ['apples', 'oranges', 'pears', 'strawberries', 'watermelon']
+return 
+  name1:  apples
+  name2:  oranges
+  name3:  pears
+  name4:  strawberries
+  name5:  watermelon
+  
+'''
+
+def who_what(a, b):
+    sentence = ''
+    i = 0 
+    while i <= len(a) - 1:
+        name = a[i]
+        i += 1
+        j = 0 
+        while j <= len(b) - 1: #dub while loop to specify elem. // won't work if list size is unmatched
+            fruit = b[j]
+            if b.index(fruit) == a.index(name): #needed index() to grab elems at same place
+                print(name + ' likes ' + fruit)
+            j += 1
+ 
