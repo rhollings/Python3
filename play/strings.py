4237ce487c42f45a2ec0160336314ec03b501b36
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-12-01 11:34:23 +0100

    Update strings.py
    added learned algo

--- play/strings.py
+++ play/strings.py
@@ -6,3 +6,21 @@ for str in string: #iterates thru a string
   print(str)
   if str == 'whatever':
     print('this')
+
+    
+'''
+splt and join function:
+given a string of space separated words, return a string with dash seperated words.
+
+ex. this is a string
+this-is-a-string
+'''
+
+def split_and_join(line):
+  line = line.split(" ")
+  line = "-".join(line)
+  return line
+# ====OR=====
+def split_adn_join(line):
+  line = line.replace(" ", "-")
+  return line 
