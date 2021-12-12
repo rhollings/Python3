26fad9917c7542b7f0a4fa880c9b7f500122110e
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2021-12-12 14:50:02 +0100

    Update zig_zag_fix.py

--- werk/zig_zag_fix.py
+++ werk/zig_zag_fix.py
@@ -15,15 +15,15 @@ Note: You can modify at most three lines in the given code. You cannot add or re
 
 def findZigZagSequence(a, n):
     a.sort()
-    mid = int((n + 1)/2)
+    mid = int((n + 1)/2-1)
     a[mid], a[n-1] = a[n-1], a[mid]
 
     st = mid + 1
-    ed = n - 1
+    ed = n - 2
     while(st <= ed):
         a[st], a[ed] = a[ed], a[st]
         st = st + 1
-        ed = ed + 1
+        ed = ed - 1
 
     for i in range (n):
         if i == n-1:
