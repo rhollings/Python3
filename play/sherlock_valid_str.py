c33beb876deb043e7133a29394bb59fee42313ba
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2022-01-04 23:06:06 +0100

    Update sherlock_valid_str.py

--- play/sherlock_valid_str.py
+++ play/sherlock_valid_str.py
@@ -3,6 +3,16 @@ Sherlock considers a string to be valid if all characters of the string appear t
 It is also valid if he can remove just 1 character at 1 index in the string, and the remaining characters will occur the same number of times.
 Given a string , determine if it is valid. If so, return YES, otherwise return NO.
 
+Example: 
+s = abc 
+This is a valid string because frequencies are {a:1, b:1, c:1} 
+
+s = abcc
+This is a valid string because we can remove one c and have 1 of each character in the remaining string.
+
+s= abccc
+This string is not valid as we can only remove 1 occurrence of c. That leaves character frequencies of {a:1, b:1, c:2}.
+
 
 '''
 
