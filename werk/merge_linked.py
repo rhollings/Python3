e0dc1c1ed3baf8d7daf2cd337726d61df7b48629
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2022-01-10 11:30:58 +0100

    Update merge_linked.py

--- werk/merge_linked.py
+++ werk/merge_linked.py
@@ -24,3 +24,31 @@ def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
       tail.next = l2
       
     return dummy
+
+  
+  def mergeLists(head1, head2):
+    if head1==None and head2==None:
+        return head1
+    if head1==None:
+        return head2
+    if head2==None:
+        return head1
+    
+    res=SinglyLinkedListNode(None)
+    c=res
+    c1=head1
+    c2=head2
+    while c1!=None and c2!=None:
+        if c1.data<=c2.data:
+            c.next=c1
+            c1=c1.next
+        else:
+            c.next=c2
+            c2=c2.next
+        c=c.next
+    
+    if c1!=None:
+        c.next=c1
+    if c2!=None:
+        c.next=c2
+    return res.next
