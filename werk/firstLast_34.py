d4d9b7951d7908fabc0c19723cd94e6a866a4348
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2022-04-11 14:13:09 +0200

    Update firstLast_34.py

--- werk/firstLast_34.py
+++ werk/firstLast_34.py
@@ -13,12 +13,37 @@ class Solution:
 
 #O(logn)
 class Solution:
-    def findStart():
-        pass
-    def findEnd():
-        pass
+    def find_start(nums, target):
+        if nums[0] == target:
+            return 0
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target and arr[mid-1] < target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return -1
+    
+    def find_end():
+        if nums[-1] == target:
+            return len(arr) - 1
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target and arr[mid+1] < target:
+                return mid
+            elif nums[mid] > target:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return -1
     
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        start = findStart()
-        end = findEnd()
+        if len(nums) == 0 or nums[0] > target or nums[-1] < target:
+            return [-1, -1]
+        start = find_start(nums, target)
+        end = find_end(nums, target)
         return [start, end]
