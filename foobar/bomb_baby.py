746a4b1e39bdb8ca4ac825f08115a6e0fc358d95
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2022-06-20 14:51:37 +0200

    Update bomb_baby.py

--- foobar/bomb_baby.py
+++ foobar/bomb_baby.py
@@ -1,4 +1,15 @@
 
+# MOST OPTIMAL SOLUTION 
+# O(logn)
+def solution(x, y):
+    M, F = max(int(x), int(y)), min(int(x), int(y))
+    res = 0
+    while F > 0:
+        res += M // F
+        M, F = F, M % F
+    if M != 1:
+        return 'impossible'
+    return str(res-1)
 
 '''
 Bomb, Baby!
