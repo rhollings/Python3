ad05cbe613d7dca91c1313571a2702392af65264
Roderick Hollingsworth <75183667+rhollings@users.noreply.github.com>
2022-10-13 09:55:18 -0600

    Update fibonacci.py

--- werk/fibonacci.py
+++ werk/fibonacci.py
@@ -18,3 +18,17 @@ def fib(n, memo = {}):
     return 1
   memo[n] = fib(n-1, memo) + fib(n-2, memo)
   return memo[n]
+
+'''
+Let's walk through what this algorithm does.
+fib(l) -> return 1
+fib(2)
+fib(l) -> return 1 fib(0) -> return 0 store 1 at memo[2]
+fib(3)
+fib(2) -> lookup memo[2] -> return 1 fib(l) -> return 1
+store 2 at memo[3]
+fib(4)
+fib(3) -> lookup memo[3] -> return 2 fib(2) -> lookup memo[2] -> return 1 store 3 at memo[4]
+fib(S)
+fib(4) -> lookup memo[4] -> return 3 fib(3) -> lookup memo[3] -> return 2 store 5 at memo[S]
+'''
